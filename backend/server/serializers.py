859900d39ddcdeb3371ca5adfaf3511de256c851
Jacob <jakesoto48@gmail.com>
2025-05-09 17:06:55 -0500

    changed to plural

---
+++ backend/server/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Server, Channel, Category
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = '__all__'
+
+
+class ServerSerializer(serializers.ModelSerializer):
+    num_members = serializers.SerializerMethodField()       # Triggers get method
+    channel_server = ChannelSerializer(many=True)           # Also returns all server channels
+
+    class Meta:
+        model = Server
+        exclude = ('member',)
+
+
+    def get_num_members(self, obj):
+        # Gets number of members from queryset
+        if hasattr(obj, 'num_members'):
+            return obj.num_members
+        else:
+            return None
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        num_members = self.context.get('num_members')
+        if not num_members:
+            data.pop('num_members', None)
+        return data
+
+
